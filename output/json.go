ed937e52b0957e2ce37368b66a5abb4fdfc6233f
genghongfei <genghongfei@yidian-inc.com>
2022-10-31 15:46:09 +0800

    优化输出

--- output/json.go
+++ output/json.go
@@ -1,21 +1,24 @@
 package output
 
 import (
-	"github.com/eoe2005/g/gerror"
+	"net/http"
+
 	"github.com/gin-gonic/gin"
 )
 
 func JsonFail(g *gin.Context, code int, msg string) {
-	panic(gerror.JsonError{
-		Code: code,
-		Msg:  msg,
-		Data: gin.H{},
+	g.AbortWithStatusJSON(http.StatusOK, gin.H{
+		"code": code,
+		"msg":  msg,
+		"data": gin.H{},
 	})
+	g.Done()
 }
 func JsonSuccess(g *gin.Context, data interface{}) {
-	panic(gerror.JsonError{
-		Code: 0,
-		Msg:  "success",
-		Data: gin.H{},
+	g.AbortWithStatusJSON(http.StatusOK, gin.H{
+		"code": 0,
+		"msg":  "success",
+		"data": data,
 	})
+	g.Done()
 }
