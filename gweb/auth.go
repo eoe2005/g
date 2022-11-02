6d9efe042f65faa24ea6de3674c5d193b60ebcdd
genghongfei <genghongfei@yidian-inc.com>
2022-11-02 14:20:58 +0800

    update:jin auth

--- gweb/auth.go
+++ gweb/auth.go
@@ -24,112 +24,98 @@ func getAuthMiddleWare(conf *gconf.GWebAuthYaml) gin.HandlerFunc {
 }
 func getJwtMiddleWare(conf *gconf.GWebAuthYaml) gin.HandlerFunc {
 	return func(ctx *gin.Context) {
-		data := ""
-		if conf.IsHeader {
-			data = ctx.GetHeader(conf.SendName)
-		}
-		if data == "" && conf.IsCookie {
-			data, _ = ctx.Cookie(conf.SendName)
-		}
-		sess := GSession{}
-		if data != "" {
-			data, err := aes.AesCbcDecryptByBase64(data, []byte(conf.AuthKey), nil)
-			if err == nil {
-				json.Unmarshal(data, sess)
+		buildAuthMiddleWare(conf, ctx, func(s *string, sess GSession, gay *gconf.GWebAuthYaml, ctx *gin.Context) {
+			if *s != "" {
+				data, err := aes.AesCbcDecryptByBase64(*s, []byte(conf.AuthKey), nil)
+				if err == nil {
+					json.Unmarshal(data, sess)
+				}
 			}
+		}, func(s *string, sess GSession, gay *gconf.GWebAuthYaml, ctx *gin.Context) {
+			sdm, e := json.Marshal(sess)
+			if e != nil {
+				return
+			}
+			od, e := aes.AesCbcEncryptBase64(sdm, []byte(conf.AuthKey), nil)
+			if e != nil {
+				return
+			}
+			*s = od
+		})
 
-		}
-		ctx.Set("session", sess)
-		ctx.Next()
-
-		sdm, e := json.Marshal(sess)
-		if e != nil {
-			return
-		}
-		od, e := aes.AesCbcEncryptBase64(sdm, []byte(conf.AuthKey), nil)
-		if e != nil {
-			return
-		}
-		if conf.IsHeader {
-			ctx.Writer.Header().Add(conf.SendName, od)
-		}
-		if conf.IsCookie {
-			ctx.SetCookie(conf.SendName, od, conf.TimeOut, "/", "", false, false)
-		}
 	}
 
 }
 func getRedisClusterMiddleWare(conf *gconf.GWebAuthYaml) gin.HandlerFunc {
 	return func(ctx *gin.Context) {
-		sid := ""
-		if conf.IsHeader {
-			sid = ctx.GetHeader(conf.SendName)
-		}
-		if sid == "" && conf.IsCookie {
-			sid, _ = ctx.Cookie(conf.SendName)
-		}
-		sess := GSession{}
-		if sid != "" {
-			data, e := gcache.GetRedisCluster(conf.Ref).HGetAll(ctx, "sess:"+sid).Result()
-			if e == nil {
-				for k, v := range data {
-					sess.Set(k, v)
+		buildAuthMiddleWare(conf, ctx, func(s *string, sess GSession, gay *gconf.GWebAuthYaml, ctx *gin.Context) {
+			if *s == "" {
+				*s = uuid.New().String()
+			} else {
+				data, e := gcache.GetRedisCluster(conf.Ref).HGetAll(ctx, "sess:"+*s).Result()
+				if e == nil {
+					for k, v := range data {
+						sess.Set(k, v)
+					}
 				}
 			}
-		} else {
-			sid = uuid.New().String()
-		}
-		ctx.Set("session", sess)
-		ctx.Next()
-		vals := []any{}
-		for k, v := range sess {
-			vals = append(vals, k, v)
-		}
-		gcache.GetRedisCluster(conf.Ref).HMSet(ctx, "sess:"+sid, vals...)
-		gcache.GetRedisCluster(conf.Ref).Expire(ctx, "sess:"+sid, time.Duration(conf.TimeOut)*time.Second)
-		if conf.IsHeader {
-			ctx.Writer.Header().Add(conf.SendName, sid)
-		}
-		if conf.IsCookie {
-			ctx.SetCookie(conf.SendName, sid, conf.TimeOut, "/", "", false, false)
-		}
+		}, func(s *string, sess GSession, gay *gconf.GWebAuthYaml, ctx *gin.Context) {
+			vals := []any{}
+			for k, v := range sess {
+				vals = append(vals, k, v)
+			}
+			gcache.GetRedisCluster(conf.Ref).HMSet(ctx, "sess:"+*s, vals...)
+			gcache.GetRedisCluster(conf.Ref).Expire(ctx, "sess:"+*s, time.Duration(conf.TimeOut)*time.Second)
+		})
+
 	}
 
 }
 
 func getRedisMiddleWare(conf *gconf.GWebAuthYaml) gin.HandlerFunc {
 	return func(ctx *gin.Context) {
-		sid := ""
-		if conf.IsHeader {
-			sid = ctx.GetHeader(conf.SendName)
-		}
-		if sid == "" && conf.IsCookie {
-			sid, _ = ctx.Cookie(conf.SendName)
-		}
-		sess := GSession{}
-		if sid != "" {
-			data, e := gcache.GetRedis(conf.Ref).HGetAll(ctx, "sess:"+sid).Result()
-			if e == nil {
-				for k, v := range data {
-					sess.Set(k, v)
+		buildAuthMiddleWare(conf, ctx, func(s *string, sess GSession, gay *gconf.GWebAuthYaml, ctx *gin.Context) {
+			if *s == "" {
+				*s = uuid.New().String()
+			} else {
+				data, e := gcache.GetRedis(conf.Ref).HGetAll(ctx, "sess:"+*s).Result()
+				if e == nil {
+					for k, v := range data {
+						sess.Set(k, v)
+					}
 				}
 			}
-		} else {
-			sid = uuid.New().String()
-		}
-		ctx.Set("session", sess)
-		ctx.Next()
-		vals := []any{}
-		for k, v := range sess {
-			vals = append(vals, k, v)
-		}
-		gcache.GetRedis(conf.Ref).HMSet(ctx, "sess:"+sid, vals...)
-		gcache.GetRedis(conf.Ref).Expire(ctx, "sess:"+sid, time.Duration(conf.TimeOut)*time.Second)
-		if conf.IsHeader {
-			ctx.Writer.Header().Add(conf.SendName, sid)
-		}
-		if conf.IsCookie {
-			ctx.SetCookie(conf.SendName, sid, conf.TimeOut, "/", "", false, false)
-		}
+		}, func(s *string, sess GSession, gay *gconf.GWebAuthYaml, ctx *gin.Context) {
+			vals := []any{}
+			for k, v := range sess {
+				vals = append(vals, k, v)
+			}
+			gcache.GetRedis(conf.Ref).HMSet(ctx, "sess:"+*s, vals...)
+			gcache.GetRedis(conf.Ref).Expire(ctx, "sess:"+*s, time.Duration(conf.TimeOut)*time.Second)
+		})
+
+	}
+}
+
+func buildAuthMiddleWare(conf *gconf.GWebAuthYaml, ctx *gin.Context, befor, after func(*string, GSession, *gconf.GWebAuthYaml, *gin.Context)) {
+	sid := ""
+	if conf.IsHeader {
+		sid = ctx.GetHeader(conf.SendName)
+	}
+	if sid == "" && conf.IsCookie {
+		sid, _ = ctx.Cookie(conf.SendName)
+	}
+	sess := GSession{}
+	befor(&sid, sess, conf, ctx)
+
+	ctx.Set("session", sess)
+	ctx.Next()
+	after(&sid, sess, conf, ctx)
+
+	if conf.IsHeader {
+		ctx.Writer.Header().Add(conf.SendName, sid)
+	}
+	if conf.IsCookie {
+		ctx.SetCookie(conf.SendName, sid, conf.TimeOut, "/", "", false, false)
 	}
 }
