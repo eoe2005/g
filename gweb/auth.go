036a46aec1622d1ce90daedf2d52481d33416e97
genghongfei <genghongfei@yidian-inc.com>
2022-11-07 21:30:19 +0800

    update:gine middle

--- gweb/auth.go
+++ gweb/auth.go
@@ -14,109 +14,104 @@ import (
 )
 
 func initJwtMiddleWare(conf *gconf.GWebMiddleWareYaml) gin.HandlerFunc {
-	return func(ctx *gin.Context) {
-		buildAuthMiddleWare(conf, ctx, func(s *string, sess *GSession, gay *gconf.GWebMiddleWareYaml, ctx *gin.Context) {
-			if *s != "" {
-				data, err := aes.AesCbcDecryptByBase64(*s, []byte(conf.AuthKey), nil)
-				if err == nil {
-					ee := json.Unmarshal(data, sess)
-					if ee != nil {
-						glog.Debug(ctx, "gwt %s -> %s -> %v : %s", *s, string(data), *sess, ee.Error())
-					}
-					glog.Debug(ctx, "gwt %s -> %s -> %v ", *s, string(data), *sess)
+	return buildAuthMiddleWare(conf, func(s *string, sess *GSession, gay *gconf.GWebMiddleWareYaml, ctx *gin.Context) {
+		if *s != "" {
+			data, err := aes.AesCbcDecryptByBase64(*s, []byte(conf.AuthKey), nil)
+			if err == nil {
+				ee := json.Unmarshal(data, sess)
+				if ee != nil {
+					glog.Debug(ctx, "gwt %s -> %s -> %v : %s", *s, string(data), *sess, ee.Error())
 				}
+				glog.Debug(ctx, "gwt %s -> %s -> %v ", *s, string(data), *sess)
 			}
-		}, func(s *string, sess *GSession, gay *gconf.GWebMiddleWareYaml, ctx *gin.Context) {
-			sdm, e := json.Marshal(sess)
-			glog.Debug(ctx, "shezhi neir %s", string(sdm))
-			if e != nil {
-				return
-			}
-			od, e := aes.AesCbcEncryptBase64(sdm, []byte(conf.AuthKey), nil)
-			if e != nil {
-				return
-			}
-			*s = od
-		})
-
-	}
+		}
+	}, func(s *string, sess *GSession, gay *gconf.GWebMiddleWareYaml, ctx *gin.Context) {
+		sdm, e := json.Marshal(sess)
+		glog.Debug(ctx, "shezhi neir %s", string(sdm))
+		if e != nil {
+			return
+		}
+		od, e := aes.AesCbcEncryptBase64(sdm, []byte(conf.AuthKey), nil)
+		if e != nil {
+			return
+		}
+		*s = od
+	})
 
 }
 func initRedisClusterMiddleWare(conf *gconf.GWebMiddleWareYaml) gin.HandlerFunc {
-	return func(ctx *gin.Context) {
-		buildAuthMiddleWare(conf, ctx, func(s *string, sess *GSession, gay *gconf.GWebMiddleWareYaml, ctx *gin.Context) {
-			if *s == "" {
-				*s = uuid.New().String()
-			} else {
-				data, e := gcache.GetRedisCluster(conf.Ref).HGetAll(ctx, "sess:"+*s).Result()
-				if e == nil {
-					for k, v := range data {
-						sess.Set(k, v)
-					}
+	return buildAuthMiddleWare(conf, func(s *string, sess *GSession, gay *gconf.GWebMiddleWareYaml, ctx *gin.Context) {
+		if *s == "" {
+			*s = uuid.New().String()
+		} else {
+			data, e := gcache.GetRedisCluster(conf.Ref).HGetAll(ctx, "sess:"+*s).Result()
+			if e == nil {
+				for k, v := range data {
+					sess.Set(k, v)
 				}
 			}
-		}, func(s *string, sess *GSession, gay *gconf.GWebMiddleWareYaml, ctx *gin.Context) {
-			vals := []any{}
-			for k, v := range *sess {
-				vals = append(vals, k, v)
-			}
-			gcache.GetRedisCluster(conf.Ref).HMSet(ctx, "sess:"+*s, vals...)
-			gcache.GetRedisCluster(conf.Ref).Expire(ctx, "sess:"+*s, time.Duration(conf.TimeOut)*time.Second)
-		})
-
-	}
+		}
+	}, func(s *string, sess *GSession, gay *gconf.GWebMiddleWareYaml, ctx *gin.Context) {
+		vals := []any{}
+		for k, v := range *sess {
+			vals = append(vals, k, v)
+		}
+		gcache.GetRedisCluster(conf.Ref).HMSet(ctx, "sess:"+*s, vals...)
+		gcache.GetRedisCluster(conf.Ref).Expire(ctx, "sess:"+*s, time.Duration(conf.TimeOut)*time.Second)
+	})
 
 }
 
 func initRedisMiddleWare(conf *gconf.GWebMiddleWareYaml) gin.HandlerFunc {
-	return func(ctx *gin.Context) {
-		buildAuthMiddleWare(conf, ctx, func(s *string, sess *GSession, gay *gconf.GWebMiddleWareYaml, ctx *gin.Context) {
-			if *s == "" {
-				*s = uuid.New().String()
-			} else {
-				data, e := gcache.GetRedis(conf.Ref).HGetAll(ctx, "sess:"+*s).Result()
-				if e == nil {
-					for k, v := range data {
-						sess.Set(k, v)
-					}
+	return buildAuthMiddleWare(conf, func(s *string, sess *GSession, gay *gconf.GWebMiddleWareYaml, ctx *gin.Context) {
+		if *s == "" {
+			*s = uuid.New().String()
+		} else {
+			data, e := gcache.GetRedis(conf.Ref).HGetAll(ctx, "sess:"+*s).Result()
+			if e == nil {
+				for k, v := range data {
+					sess.Set(k, v)
 				}
 			}
-		}, func(s *string, sess *GSession, gay *gconf.GWebMiddleWareYaml, ctx *gin.Context) {
-			vals := []any{}
-			for k, v := range *sess {
-				vals = append(vals, k, v)
-			}
-			gcache.GetRedis(conf.Ref).HMSet(ctx, "sess:"+*s, vals...)
-			gcache.GetRedis(conf.Ref).Expire(ctx, "sess:"+*s, time.Duration(conf.TimeOut)*time.Second)
-		})
+		}
+	}, func(s *string, sess *GSession, gay *gconf.GWebMiddleWareYaml, ctx *gin.Context) {
+		vals := []any{}
+		for k, v := range *sess {
+			vals = append(vals, k, v)
+		}
+		gcache.GetRedis(conf.Ref).HMSet(ctx, "sess:"+*s, vals...)
+		gcache.GetRedis(conf.Ref).Expire(ctx, "sess:"+*s, time.Duration(conf.TimeOut)*time.Second)
+	})
 
-	}
 }
 
-func buildAuthMiddleWare(conf *gconf.GWebMiddleWareYaml, ctx *gin.Context, befor, after func(*string, *GSession, *gconf.GWebMiddleWareYaml, *gin.Context)) {
-	sid := ""
-	if conf.IsHeader {
-		sid = ctx.GetHeader(conf.SendName)
-	}
-	if sid == "" && conf.IsCookie {
-		sid, _ = ctx.Cookie(conf.SendName)
-		sid, _ = url.QueryUnescape(sid)
+func buildAuthMiddleWare(conf *gconf.GWebMiddleWareYaml, befor, after func(*string, *GSession, *gconf.GWebMiddleWareYaml, *gin.Context)) gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		sid := ""
+		if conf.IsHeader {
+			sid = ctx.GetHeader(conf.SendName)
+		}
+		if sid == "" && conf.IsCookie {
+			sid, _ = ctx.Cookie(conf.SendName)
+			sid, _ = url.QueryUnescape(sid)
+		}
+
+		glog.Debug(ctx, "read sid %s", sid)
+		sess := &GSession{}
+		befor(&sid, sess, conf, ctx)
+
+		ctx.Set("session", sess)
+		ctx.Next()
+		after(&sid, sess, conf, ctx)
+		glog.Debug(ctx, "设置 sid %s", sid)
+		if conf.IsHeader {
+			ctx.Writer.Header().Add(conf.SendName, sid)
+			ctx.Header(conf.SendName, sid)
+		}
+		if conf.IsCookie {
+			glog.Debug(ctx, "设置cookie sid %s", sid)
+			ctx.SetCookie(conf.SendName, sid, conf.TimeOut, "/", "", false, true)
+		}
 	}
 
-	glog.Debug(ctx, "read sid %s", sid)
-	sess := &GSession{}
-	befor(&sid, sess, conf, ctx)
-
-	ctx.Set("session", sess)
-	ctx.Next()
-	after(&sid, sess, conf, ctx)
-	glog.Debug(ctx, "设置 sid %s", sid)
-	if conf.IsHeader {
-		ctx.Writer.Header().Add(conf.SendName, sid)
-		ctx.Header(conf.SendName, sid)
-	}
-	if conf.IsCookie {
-		glog.Debug(ctx, "设置cookie sid %s", sid)
-		ctx.SetCookie(conf.SendName, sid, conf.TimeOut, "/", "", false, true)
-	}
 }
--- gweb/encrypt.go
+++ gweb/encrypt.go
@@ -23,46 +23,44 @@ func (w *gineEncryptWriter) WriteString(s string) (int, error) {
 }
 
 func initAesMiddleWare(conf *gconf.GWebMiddleWareYaml) gin.HandlerFunc {
-	return func(ctx *gin.Context) {
-		resetInput(conf, ctx, func(b []byte, gey *gconf.GWebMiddleWareYaml) []byte {
-			r, _ := aes.AesCbcDecryptByBase64(string(b), []byte(gey.Key), nil)
-			return r
-		}, func(b []byte, gey *gconf.GWebMiddleWareYaml) string {
-			r, _ := aes.AesCbcEncryptBase64([]byte(b), []byte(gey.Key), nil)
-			return r
-		})
+	return resetInput(conf, func(b []byte, gey *gconf.GWebMiddleWareYaml) []byte {
+		r, _ := aes.AesCbcDecryptByBase64(string(b), []byte(gey.Key), nil)
+		return r
+	}, func(b []byte, gey *gconf.GWebMiddleWareYaml) string {
+		r, _ := aes.AesCbcEncryptBase64([]byte(b), []byte(gey.Key), nil)
+		return r
+	})
 
-	}
 }
 
 func initRsaMiddleWare(conf *gconf.GWebMiddleWareYaml) gin.HandlerFunc {
-	return func(ctx *gin.Context) {
-		resetInput(conf, ctx, func(b []byte, gey *gconf.GWebMiddleWareYaml) []byte {
-			r, _ := rsa.RsaDecryptByBase64(string(b), gey.PrivateKey)
-			return r
-		}, func(b []byte, gey *gconf.GWebMiddleWareYaml) string {
-			r, _ := rsa.RsaEncryptToBase64(b, gey.PublicKey)
-			return r
-		})
+	return resetInput(conf, func(b []byte, gey *gconf.GWebMiddleWareYaml) []byte {
+		r, _ := rsa.RsaDecryptByBase64(string(b), gey.PrivateKey)
+		return r
+	}, func(b []byte, gey *gconf.GWebMiddleWareYaml) string {
+		r, _ := rsa.RsaEncryptToBase64(b, gey.PublicKey)
+		return r
+	})
 
-	}
 }
 
-func resetInput(conf *gconf.GWebMiddleWareYaml, ctx *gin.Context, before func([]byte, *gconf.GWebMiddleWareYaml) []byte, after func([]byte, *gconf.GWebMiddleWareYaml) string) {
-	data, e := ioutil.ReadAll(ctx.Request.Body)
-	if e != nil {
-		idata := before(data, conf)
-		ctx.Request.Write(bytes.NewBuffer(idata))
+func resetInput(conf *gconf.GWebMiddleWareYaml, before func([]byte, *gconf.GWebMiddleWareYaml) []byte, after func([]byte, *gconf.GWebMiddleWareYaml) string) gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		data, e := ioutil.ReadAll(ctx.Request.Body)
+		if e != nil {
+			idata := before(data, conf)
+			ctx.Request.Write(bytes.NewBuffer(idata))
+		}
+		oldWriter := ctx.Writer
+		blw := &gineEncryptWriter{body: bytes.NewBufferString(""), ResponseWriter: ctx.Writer}
+		ctx.Writer = blw
+		ctx.Next()
+		responseByte := blw.body.Bytes()
+
+		ctx.Writer = oldWriter
+		base64Text := after(responseByte, conf)
+
+		ctx.Writer.WriteString(base64Text)
 	}
-	oldWriter := ctx.Writer
-	blw := &gineEncryptWriter{body: bytes.NewBufferString(""), ResponseWriter: ctx.Writer}
-	ctx.Writer = blw
-	ctx.Next()
-	responseByte := blw.body.Bytes()
-
-	ctx.Writer = oldWriter
-	base64Text := after(responseByte, conf)
-
-	ctx.Writer.WriteString(base64Text)
 
 }
