28537d6a9b6e9c75d8271f371f374be90dd454e9
eoe2005 <eoe2005@hotmail.com>
2022-11-22 01:06:03 +0800

    增加tpl末班工呢

--- output/tpl.go
+++ output/tpl.go
@@ -1,7 +1,34 @@
 package output
 
-import "github.com/gin-gonic/gin"
+import (
+	"embed"
+	"html/template"
+
+	"github.com/gin-gonic/gin"
+)
+
+var (
+	_tpl = template.New("g")
+)
 
 func Html(c *gin.Context, name string, data any) {
+	_tpl.ExecuteTemplate(c.Writer, name, data)
+	c.Abort()
+}
 
+func SetFs(prefix string, fs embed.FS) {
+	ds, e := fs.ReadDir(".")
+	if e != nil {
+		return
+	}
+	for _, f := range ds {
+		if f.IsDir() {
+		} else {
+			ts, e := fs.ReadFile(f.Name())
+			if e != nil {
+				continue
+			}
+			_tpl.New(prefix + "." + f.Name()).Parse(string(ts))
+		}
+	}
 }
