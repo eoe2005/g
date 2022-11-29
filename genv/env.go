6b6951bcd6449fcac2075dba143e11dfeeefbada
eoe2005 <eoe2005@hotmail.com>
2022-11-29 21:23:00 +0800

    conf

--- genv/env.go
+++ genv/env.go
@@ -13,12 +13,9 @@ func GetAppDir() string {
 	if _appDir == "" {
 		filePath, _ := filepath.Abs(os.Args[0])
 		_appDir = filepath.Dir(filePath)
+
 	}
 	return _appDir
-}
-
-func GetAppConfFile(name string) string {
-	return GetAppDir() + "/../conf/" + name + ".yaml"
 }
 
 func GetLogDir() string {
