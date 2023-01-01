5b4f0c951f9255a2d82a1d4d8afc66a5be70447b
eoe2005 <eoe2005@hotmail.com>
2023-01-01 23:48:08 +0800

    bugfix

--- gdb/mysql.go
+++ gdb/mysql.go
@@ -1,9 +1,11 @@
 package gdb
 
 import (
+	"context"
 	"fmt"
 
 	"github.com/eoe2005/g/gconf"
+	"github.com/eoe2005/g/glog"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
@@ -11,6 +13,7 @@ import (
 func connectMysql(dbconf *gconf.GDbYaml) *gorm.DB {
 	db, err := gorm.Open(mysql.Open(fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?charset=utf8mb4&parseTime=True&loc=Local", dbconf.UserName, dbconf.UserPass, dbconf.Host, dbconf.Port, dbconf.DbName)), &gorm.Config{})
 	if err != nil {
+		glog.Error(context.Background(), "链接数据库失败[%s]%s", fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?charset=utf8mb4&parseTime=True&loc=Local", dbconf.UserName, dbconf.UserPass, dbconf.Host, dbconf.Port, dbconf.DbName), err.Error())
 		return nil
 	}
 	return db
