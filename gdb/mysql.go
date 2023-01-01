b6f2b143b5217b815e5895e8181a9105bb7b6c58
eoe2005 <eoe2005@hotmail.com>
2023-01-02 00:16:13 +0800

    sql log

--- gdb/mysql.go
+++ gdb/mysql.go
@@ -13,9 +13,8 @@ import (
 
 func connectMysql(dbconf *gconf.GDbYaml) *gorm.DB {
 	db, err := gorm.Open(mysql.Open(fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?charset=utf8mb4&parseTime=True&loc=Local", dbconf.UserName, dbconf.UserPass, dbconf.Host, dbconf.Port, dbconf.DbName)), &gorm.Config{
-		PrepareStmt:            true,
-		SkipDefaultTransaction: true,
-		Logger:                 logger.New(glog.GetLog("sql"), logger.Config{}),
+		PrepareStmt: true,
+		Logger:      logger.New(glog.GetLog("sql"), logger.Config{}),
 	})
 	if err != nil {
 		glog.Error(context.Background(), "链接数据库失败[%s]%s", fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?charset=utf8mb4&parseTime=True&loc=Local", dbconf.UserName, dbconf.UserPass, dbconf.Host, dbconf.Port, dbconf.DbName), err.Error())
